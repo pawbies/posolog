898e644354eaac0cb1ff37af88cbc6458ad418d3
pawbies <alex.fuchs@pawbies.net>
2026-07-28 17:41:37 +0200

    Created basic tab navigation

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,47 @@
+import { NativeTabs } from "expo-router/unstable-native-tabs";
+
+export default function TabsLayout() {
+    return (
+        <NativeTabs>
+            <NativeTabs.Trigger name="index">
+                <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon 
+                    sf={{ default: "house", selected: "house.fill" }}
+                    md="home"
+                />
+            </NativeTabs.Trigger>
+
+            <NativeTabs.Trigger name="timeline">
+                <NativeTabs.Trigger.Label>Timeline</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon
+                    sf={{ default: "clock", selected: "clock.fill" }}
+                    md="timeline"
+                />
+            </NativeTabs.Trigger>
+
+            <NativeTabs.Trigger name="medications/index">
+                <NativeTabs.Trigger.Label>Medications</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon
+                    sf={{ default: "pills", selected: "pills.fill" }}
+                    md="medication"
+                />
+            </NativeTabs.Trigger>
+
+            <NativeTabs.Trigger name="tracking">
+                <NativeTabs.Trigger.Label>Tracking</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon
+                    sf={{ default: "heart", selected: "heart.fill" }}
+                    md="heart_plus"
+                />
+            </NativeTabs.Trigger>
+
+            <NativeTabs.Trigger name="more">
+                <NativeTabs.Trigger.Label>More</NativeTabs.Trigger.Label>
+                <NativeTabs.Trigger.Icon
+                    sf={{ default: "ellipsis", selected: "ellipsis.circle" }}
+                    md="more"
+                />
+            </NativeTabs.Trigger>
+        </NativeTabs>
+    );
+}
---
+++ src/app/(tabs)/timeline.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function TimelineScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white dark:bg-black">
+      <Text className="text-3xl text-black dark:text-white">Timeline</Text>
+    </View>
+  );
+}
