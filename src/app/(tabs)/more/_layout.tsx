6497f35c61ac5816cb765f099699167f7ac723cd
pawbies <alex.fuchs@pawbies.net>
2026-08-01 12:51:32 +0200

    Moved theme provider to root layout

--- src/app/(tabs)/more/_layout.tsx
+++ src/app/(tabs)/more/_layout.tsx
@@ -1,23 +1,18 @@
 import "@/styles/global.css";
-import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
-import { useColorScheme } from "nativewind";
+import { Stack } from "expo-router";
 
 export default function MoreLayout() {
-  const { colorScheme } = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ animation: "default" }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="calculator" options={{ title: "Calculator" }} />
-        <Stack.Screen name="statistics" options={{ title: "Statistics" }} />
-        <Stack.Screen name="import" options={{ title: "Import" }} />
-        <Stack.Screen name="export" options={{ title: "Export" }} />
-        <Stack.Screen name="settings" options={{ title: "Settings" }} />
-        <Stack.Screen name="wiki" options={{ title: "Wiki" }} />
-        <Stack.Screen name="donate" options={{ title: "Donate" }} />
-        <Stack.Screen name="about" options={{ title: "About" }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack screenOptions={{ animation: "default" }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="calculator" options={{ title: "Calculator" }} />
+      <Stack.Screen name="statistics" options={{ title: "Statistics" }} />
+      <Stack.Screen name="import" options={{ title: "Import" }} />
+      <Stack.Screen name="export" options={{ title: "Export" }} />
+      <Stack.Screen name="settings" options={{ title: "Settings" }} />
+      <Stack.Screen name="wiki" options={{ title: "Wiki" }} />
+      <Stack.Screen name="donate" options={{ title: "Donate" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+    </Stack>
   )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,15 @@
 import "@/styles/global.css";
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
+import { useColorScheme } from "nativewind";
 
 export default function RootLayout() {
+  const { colorScheme } = useColorScheme();
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
   )
 }
